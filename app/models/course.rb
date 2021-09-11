3043c4feb8ff4bdc7935d9c283f1d09219e94c8f
baothi <baothi246@gmail.com>
2021-09-11 20:13:29 +0700

    add gem friendly_id

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,4 +8,20 @@ class Course < ApplicationRecord
     title
   end
   has_rich_text :description
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  
+  # friendly_id :generated_slug, use: :slugged
+
+  # def generated_slug
+  #   required 'securerandom'
+  #   @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
+  # end
+  
+  # def to_s
+  #   slug
+  # end
+
+
 end
